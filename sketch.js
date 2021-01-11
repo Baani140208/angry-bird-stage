ca3248e612226bb44732f7bec0f15069edcac020
Baani140208 <baani6763@gmail.com>
2021-01-11 16:34:49 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,pig4;
 var backgroundImg,platform;
 
 function preload() {
@@ -17,11 +17,12 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(150, 300, 300, 150);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
+    pig4 = new Pig(200,200);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
@@ -48,6 +49,7 @@ function draw(){
     box2.display();
     ground.display();
     pig1.display();
+    pig4.display();
     log1.display();
 
     box3.display();
